6ebb57d3e55a18665c198f98de185df5f76363b1
harveyspectar <ultimatepravin72@gmail.com>
2017-07-25 12:28:05 +0200

    serranodsdd: JDC-ify

--- aosp_serranodsdd.mk
+++ aosp_serranodsdd.mk
@@ -20,9 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/serranodsdd/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_serranodsdd
+PRODUCT_NAME := jdc_serranodsdd
 PRODUCT_DEVICE := serranodsdd
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
